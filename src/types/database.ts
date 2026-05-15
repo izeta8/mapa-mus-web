6ad90bf3851694430900cd396c40cf469c3c8657
izeta8 <jiz.j12.a4@gmail.com>
2026-05-15 19:04:48 +0200

    feat: update database types.

--- src/types/database.ts
+++ src/types/database.ts
@@ -3,7 +3,7 @@ import { Database } from '@/types/supabase'
 export type ViewMode = "lista" | "bracket";
 
 export type Match = Database['public']['Tables']['matches']['Row']
-export type Tournament = Database['public']['Tables']['tournaments']['Row']
+export type Tournament = Database['public']['Tables']['tournaments_develop']['Row']
 export type Couple = Database['public']['Tables']['couples']['Row']
 
 export type MatchInsert = Database['public']['Tables']['matches']['Insert']
