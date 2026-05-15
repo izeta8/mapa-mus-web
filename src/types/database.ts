dcee5a122f8b2b80324ac98c24c76fc2e792f9f2
izeta8 <jiz.j12.a4@gmail.com>
2026-05-15 20:22:33 +0200

    feat: update supabase types

--- src/types/database.ts
+++ src/types/database.ts
@@ -1,6 +1,6 @@
 import { Database } from '@/types/supabase'
 
-export type ViewMode = "lista" | "bracket";
+export type ViewMode = "matchup" | "bracket";
 
 export type Match = Database['public']['Tables']['matches']['Row']
 export type Tournament = Database['public']['Tables']['tournaments_develop']['Row']
@@ -8,3 +8,18 @@ export type Couple = Database['public']['Tables']['couples']['Row']
 
 export type MatchInsert = Database['public']['Tables']['matches']['Insert']
 
+export interface CoupleInfo {
+  player1_name: string | null;
+  player2_name: string | null;
+  couple_number: number;
+}
+
+export interface MatchWithCouples extends Match {
+  couple1: CoupleInfo | null;
+  couple2: CoupleInfo | null;
+}
+
+export interface TournamentFull extends Tournament {
+  matches: MatchWithCouples[];
+  total_couples: number;
+}
---
+++ src/types/ui.ts
@@ -0,0 +1,8 @@
+
+export interface MatchupCardStyles {
+    container: string;
+    numero: string;
+    mesa: string;
+    vs: string;
+    badge: string;
+}
