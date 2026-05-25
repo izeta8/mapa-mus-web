ff84e6e6bf4960fdaf8523e86e3623c156066627
izeta8 <jiz.j12.a4@gmail.com>
2026-05-25 11:58:38 +0200

    refactor: make URLs spanish and organize better URLs.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/tournament/:path*',
+        destination: '/torneo/:path*',
+        permanent: true, // Esto aplica un estado HTTP 308 (Redirección permanente)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
