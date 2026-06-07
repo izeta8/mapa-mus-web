9a143375917e8f8df3031affbe7b1d2aeb5d46b4
izeta8 <jiz.j12.a4@gmail.com>
2026-06-07 18:07:20 +0200

    feat: make header bigger in TV view

--- src/app/(public-tv)/tv/[short-id]/components/TVHeader.tsx
+++ src/app/(public-tv)/tv/[short-id]/components/TVHeader.tsx
@@ -17,29 +17,29 @@ interface TVModeSelectorProps {
 }
 
 
-export default function TVHeader({tournamentName, viewMode, setViewMode, inscribedCouples, aliveCouples, isBracketCreated}: TVHeaderProps) {
+export default function TVHeader({ tournamentName, viewMode, setViewMode, inscribedCouples, aliveCouples, isBracketCreated }: TVHeaderProps) {
 
   return (
-    <header className="flex justify-between items-center shrink-0 mb-4">
-        <div className="flex items-center gap-4">
-          <div className="bg-black text-white px-4 py-2">
-            <h1 className="text-3xl font-black tracking-tight">{tournamentName}</h1>
-          </div>
-          <div className="flex flex-col">
-            <span className="text-sm font-bold text-zinc-500">
-              {isBracketCreated ? "PAREJAS VIVAS" : "TOTAL PAREJAS"}
-            </span>
-            <span className="text-2xl font-black text-zinc-700">
-              {isBracketCreated ? `${aliveCouples}/${inscribedCouples}` : inscribedCouples}
-            </span>
-          </div>
+    <header className="flex justify-between items-center shrink-0 mb-6 w-full">
+      <div className="flex items-center gap-8">
+        <div className="bg-black text-white px-6 py-3">
+          <h1 className="text-5xl font-black tracking-tight uppercase">{tournamentName}</h1>
         </div>
-     
-        {isBracketCreated && (
-          <TVModeSelector viewMode={viewMode} setViewMode={setViewMode} />
-        )}
+        <div className="flex flex-col">
+          <span className="text-base font-black text-zinc-400 tracking-wider">
+            {isBracketCreated ? "PAREJAS VIVAS" : "TOTAL PAREJAS"}
+          </span>
+          <span className="text-4xl font-black text-zinc-850">
+            {isBracketCreated ? `${aliveCouples}/${inscribedCouples}` : inscribedCouples}
+          </span>
+        </div>
+      </div>
 
-      </header>
+      {isBracketCreated && (
+        <TVModeSelector viewMode={viewMode} setViewMode={setViewMode} />
+      )}
+
+    </header>
   )
 
 }
@@ -48,28 +48,28 @@ function TVModeSelector({ viewMode, setViewMode }: TVModeSelectorProps) {
 
   return (
     <Tabs defaultValue={viewMode}>
-      <TabsList>
+      <TabsList className="py-6 bg-zinc-100 rounded-2xl transition-all">
 
         {/* PAREJAS */}
-        <TabsTrigger  
-          value="matchup" 
-          className="p-4 pointer"
+        <TabsTrigger
+          value="matchup"
+          className="px-10 py-5 text-2xl flex items-center gap-4 pointer cursor-pointer rounded-xl transition-all"
           onClick={() => setViewMode("matchup")}
         >
-          <StretchHorizontal />
+          <StretchHorizontal className="w-8 h-8 size-8 shrink-0" />
           Enfrentamientos
         </TabsTrigger>
-        
+
         {/* CUADRO */}
-        <TabsTrigger  
-          value="bracket" 
-          className="p-4 pointer"
+        <TabsTrigger
+          value="bracket"
+          className="px-10 py-5 text-xl flex items-center gap-4 pointer cursor-pointer rounded-xl transition-all"
           onClick={() => setViewMode("bracket")}
         >
-          <Network/>
+          <Network className="w-8 h-8 size-8 shrink-0" />
           Cuadro
         </TabsTrigger>
-      
+
       </TabsList>
     </Tabs>
   )
